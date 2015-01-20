b030340342a4184dd6a40d3d14e55a5417d743d1
Jim Muir <muir.29@osu.edu>
2015-01-20 17:23:10 -0500

    re-enabled connection.scan()

--- test/zoom.js
+++ test/zoom.js
@@ -2,7 +2,7 @@
 
 var expect = require('chai').expect;
 var zoom = require('../lib');
-var conn = new zoom.Connection('192.83.186.170:210/INNOPAC');
+var conn = new zoom.Connection('z3950.indexdata.com:210/gils');
 
 before(function (done) {
   conn.connect(function (err) {
@@ -16,7 +16,7 @@ before(function (done) {
 describe('connection', function () {
   describe('.search()', function () {
     it('should run widthout error', function (done) {
-      var query = new zoom.Query('@attr 1=4 台灣');
+      var query = new zoom.Query('@attr 1=4 book');
       conn.search(query, function (err) {
         expect(err).to.be.null;
         done();
@@ -24,13 +24,13 @@ describe('connection', function () {
     });
   });
 
-  /*describe('.scan()', function () {
+  describe('.scan()', function () {
     it('should run widthout error', function (done) {
-      var query = new zoom.Query('@attr 1=4 台灣');
+      var query = new zoom.Query('@attr 1=4 book');
       conn.scan(query, function (err) {
         expect(err).to.be.null;
         done();
       });
     });
-  });*/
+  });
 });
